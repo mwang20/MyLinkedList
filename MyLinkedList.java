e0481d6dba908570cd292bfa61ea2b099a72b851
mwang20 <mwang20@stuy.edu>
2021-01-08 09:38:50 -0500

    Created MyLinkedList constructor.

---
+++ MyLinkedList.java
@@ -0,0 +1,16 @@
+public class MyLinkedList extends Node{
+  private int size;
+  private Node start,end;  
+
+  public MyLinkedList(){
+    size = 0;
+    start = end = null;
+  }
+  public int size(){};
+  public boolean add(String value);
+  public boolean add(int index, String value);
+  public String get(int index);
+  public String set(int index, String value);
+  public String toString();
+
+}
