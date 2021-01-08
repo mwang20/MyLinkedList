cb5c84ed6476323ca302f2c8f734f1ee0a6801db
mwang20 <mwang20@stuy.edu>
2021-01-08 09:58:52 -0500

    I created add(String value) method

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -9,7 +9,20 @@ public class MyLinkedList extends Node{
   public int size()(
     return size;
   )
-  public boolean add(String value);
+  public boolean add(String value){
+    Node node = new Node(value);
+    if (size == 0){
+      start = end = node;
+      start.setPrev(null);
+      start.setNext(null);
+      size += 1;
+    }
+    else {
+      end.setNext(node);
+      end = node
+      size++;
+    }
+  };
   public boolean add(int index, String value);
   public String get(int index);
   public String set(int index, String value);
